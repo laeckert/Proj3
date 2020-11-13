93e3c0ef246f19ce6fd9659ba4d96308952d2377
laeckert <laeckert@ncsu.edu>
2020-11-12 22:00:02 -0500

    fixed a bunch of stuff

--- server.R
+++ server.R
@@ -1,71 +1,234 @@
-
+#load in packages
 library(shiny)
-library(markdown)
+library(shinydashboard)
+library(readr)
+library(devtools)
+library(dplyr)
 library(DT)
 library(ggplot2)
-library(dplyr)
-library(plotly)
-library(shinydashboard)
-library(shiny)
-library(tidyverse)
-library(markdown)
-library(maps)
+library(caret)
+library(ggfortify)
 
-
-#Load in our data 
-happydata <- read_csv("2016.csv")
-regional <- happydata %>% group_by(Region)
+#Load in our data and filter by year
+data <- as_tibble(read_csv("2016.csv"))
+pdata <- as_tibble(read_csv("2016.csv"))
 
 #Set up URLs 
 url <- a("My github page", href="https://github.com/laeckert/Proj3")
-urldata <- a("Dataset", href="https://www.kaggle.com/unsdsn/world-happiness?select=2016.csv")
+urldata <- a("Dataset", href="https://www.kaggle.com/unsdsn/world-happiness")
 
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output, session){
   
+  #Link URLs to the dataset and personal github page. 
   output$datatab <- renderUI({
     tagList("Here you can find the dataset:", urldata)
   })
+  
   output$tab <- renderUI({
-    tagList("Here you can see visit my github page to see code:", url)
+    tagList("Visit my github page to see code:", url)
+    
   })
   
-  #downloadable csv of dataset
-  output$downloadData <- downloadHandler(
-    filename = function() {
-      paste(region, ".csv", sep = "")
-    },
+  #Get our Filtered Data dynamically. 
+  getData <- reactive({
+    newData <- pdata %>% filter(pdata$Region == input$causes)
+  })
+  getData1 <- reactive({
+    newData <- pdata %>% filter(pdata$Region == input$cause)
+  })
+  getData2 <- reactive({
+    newData <- pdata %>% filter(pdata$Region == input$cauze)
+  })
+  
+  #Dynamic UI that enables user to reset to default settings
+  observe({
+    if(input$check){
+      updateSliderInput(session, "size", min=1, max=10, value=1)
+      updateTextInput(session, "info")
+    }
+    else
+      updateSliderInput(session, "size", min=1, max=10, value=5)
+  })
+  
+  #create plot
+  output$dataplot <- renderPlot({
+    
+    #get filtered data
+    newData <- getData()
+    #create plot
+    g <- ggplot(newData, aes(x=Health, y=Score)) 
+    g + geom_point(size=input$size, aes(col=input$causes))
+  })
+  
+  #Make a reactive plot. 
+  plotInput <- reactive({
+    #get filtered data
+    newData <- getData()
+    
+    #create plot
+    g <- ggplot(newData, aes(x=Health, y=Score)) 
+    g + geom_point(size=input$size, aes(col=input$causes))
+  })
+  
+  #Make the plot downloadable to a png file. 
+  output$DownloadPlot <- downloadHandler(
+    filename = function() { paste(input$causes, '.png', sep='') },
     content = function(file) {
-      write.csv(region, file, row.names = FALSE)
+      png(file)
+      print(plotInput())
+      dev.off()
+    })
+  
+  #creating the dynamic titles for our exploration page
+  output$title <- renderUI({
+    text <- HTML("Exploring Data <br> <br> Scatterplot of Cause:",
+                 input$causes)
+  })
+  
+  #create ability for user to click on plot
+  output$info <- renderText({
+    paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+  })
+  
+  output$biplottext <- renderUI({
+    if(input$PCs=="Rank"){
+      paste0("Here we look at the linear combination of the Happiness 
+                   and the Year variables for the cause: ", input$cauze,". This biplot shows 
+                   us how strongly the principal components, Year and Happiness, influence each other, and to what extent,
+                   shown by the arrows and the points. Here we can see that the Year somewhat influences the Happiness
+                   as shown by how long the line or vector is for both components. However, it can be difficult to tell as well, 
+                   since the points are scattered in a odd-looking pattern.")
+    }
+    else if(input$PCs=="Health"){
+      paste0("Here we look at the linear combination of 2 principal components: Happiness and the 
+                   Health. The cause of death that we are looking at is: ", input$cauze,". This biplot 
+                   shows us how strongly the principal components influence each other and to what extent, 
+                   as shown by the arrows and the points. Here we can see that the Health component greatly influences the 
+                   Happiness, as the number of Health, intuitively, tells us how the death rate will be. The line or vector that is pointing out, 
+                   indicates a strong relationship, which can also be shown in the direction of the points. ")
+    }
+  })
+  
+  #Create text for model output
+  output$modeltext <- renderUI({
+    if(input$slrmodel=="Rank"){
+      paste0("It looks like that as we have the Year to measure our Happiness, we visualize an interesting 
+                   regression plot. We can view the frequency or # of occurrences with how the levels of Happiness
+                   that we have. For example, for 2015, we have Happiness relatively spread out, with the higher levels
+                   having many occurrences 150 and 250 for the Happiness. As for the previous years, we see that they are 
+                   more concentrated around a smaller interval such as 150 and 200. This may seem to indicate that as year
+                   grows, the death rate may be variable and highly unpredictable given the top 10 causes of death in the U.S.")
+    }
+    
+    else if(input$slrmodel=="Health"){
+      paste0("It looks like the number of Health appears to be a great predictor of the death rate, as expected. 
+                   Since the Death rate resembles the number of Health from a population given a certain time frame, the 
+                   Health that we have seem to be correlated with our death rate. ")
+    }
+  })
+  
+  #Get our numeric summaries
+  output$text <- renderText({
+    #get filtered data
+    newData <- getData()
+    paste("The average Health for cause", input$causes, 
+          "is", round(mean(newData$Health, na.rm = TRUE), 2), 
+          "and the average age-adjusted death rate is", 
+          round(mean(newData$Score, na.rm = TRUE), 2), sep = " ")
+  })
+  
+  #create table of output observations for data table page
+  output$Tables <- renderDataTable({
+    newdata <- getData1()
+  })
+  
+  #create table of output observations for exploration page
+  output$table <- renderDataTable({
+    newdata <- getData() 
+    print(newdata)
+  })
+  
+  #Make a button that downloads a csv file of the data
+  output$DownloadData <- downloadHandler(
+    filename = function(){
+      paste(input$cause, ".csv", sep="")
+    },
+    content = function(file){
+      write.csv(getData(), file, row.names = FALSE)
     }
   )
   
-  #plot for exploretab
-  output$boxplot <- renderPlot({
-    ggplot(data = happydata, aes(x = happydata$Region, y = happydata$`Score`)) +
-      geom_boxplot(aes(color = Region, fill = Region), alpha = 0.5) +
-      geom_point(aes(color = Region), position = position_jitter(width = .1)) +
-      labs(title = "Happiness by World Region", 
-           x = "Region", 
-           y = "Happiness Score") +
-      theme_minimal() +
-      theme(plot.title = element_text(size = rel(2.5)),
-            axis.title = element_text(size = rel(1.5)),
-            axis.text.x = element_blank())
+  #Begin making dynamic user interface for choosing models 
+  ModelData <- reactive({
+    pdata[, c(input$slrmodel, "Happiness")]
   })
   
-  #create text info
-  output$info <- renderText({
-    #get filtered data
+  #1st supervised learning model
+  #Create simple models to choose from
+  model1 <- lm(Score ~ Health, data=pdata)
+  model2 <- lm(Score ~ Rank, data=pdata)
+  model3 <- lm(Score ~ Country, data=pdata)
+  sum_mod1 <- summary(model1)
+  
+  output$plot1 <- renderPlot({
+    par(mar=c(5.1, 4.1, 0, 1))
+    plot(ModelData())
+    if(input$slrmodel=='Health'){
+      abline(a=sum_mod1$coefficients[1,1], b=sum_mod1$coefficients[2,1])
+    }
+    if(input$slrmodel=='Rank'){abline(model2)}
+    if(input$slrmodel=='Country'){abline(model3)}
+  })
+  #Create Biplots
+  output$Biplot <- renderPlot({
     
-    paste("The average happiness score for", input$choice, "is", round(mean(happydata$Score, na.rm = TRUE), 2))
+    newdata <- getData2()
+    dapcs <- prcomp(select(newdata, Score, Health))
+    yapcs <- prcomp(select(newdata, Score, Rank))
+    
+    if(input$PCs=="Health"){
+      biplot(dapcs, xlabs=rep(".", nrow(newdata)), cex=1.8, col="Green")
+    }
+    
+    else if(input$PCs=="Rank"){
+      biplot(yapcs, xlabs=rep(".", nrow(newdata)), cex=1.8, col="Blue")
+    }
   })
   
-  #table for access tab
-  output$happytable <- DT::renderDataTable({
-    happydata
+  #Predict our death rate based on # of Health. Used a sliderinput. 
+  output$mtable <- renderTable({
+    modeldata <- data.frame(Health = pdata$Health, Score = pdata$Score)
+    model_lm <- lm(Score ~ Health, data=modeldata)
+    ddata <- data.frame(Health=input$Health)
+    preddata <- predict(model_lm, ddata)
+    
   })
-
-
-
-
+  
+  #Make the models for our MLR models. 
+  model4 <- lm(Score ~ Health + Region, data=pdata)
+  model5 <- lm(Score ~ Health + Rank, data=pdata)
+  model6 <- lm(Score ~ Region + Rank, data=pdata)
+  
+  #Summaries to get our coefficients of R-Squared. 
+  sum_mod4 <- summary(model4)
+  sum_mod5 <- summary(model5)
+  sum_mod6 <- summary(model6)
+  
+  #Second supervised learning model - MLR
+  output$mlrmodel<- renderTable({
+    if(input$mlrpreds=="Effect of Health and Cause on Happiness"){
+      paste("R-squared: ", sum_mod4$r.squared, "As this is a multiple linear reg. model, we interpret the R-squared value as how the cause and year predictors are related.",
+            "As the R-squared is high, we can conclude that the # of Health and Cause are highly related in measuring the death rate.")
+    }
+    else if(input$mlrpreds=="Effect of Health and Year on Happiness"){
+      paste("R-squared: ", sum_mod5$r.squared, "As this is a multiple linear reg. model, we interpret the R-squared value as how the cause and year predictors are related.",
+            "As the R-squared is low, we can conclude that the # of Health and Year aren't very related in measuring the death rate.")
+    }
+    else if(input$mlrpreds=="Effect of Cause and Year on Happiness"){
+      paste("R-squared: ", sum_mod6$r.squared, "As this is a multiple linear reg. model, we interpret the R-squared value as how the cause and year predictors are related.",
+            "As the R-squared is high, we can conclude that the cause and year are highly related in measuring the death rate.")
+      
+    }
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,74 +1,115 @@
-
-library(DT)
-library(ggplot2)
-library(dplyr)
-library(plotly)
-library(shinythemes)
+#load in packages
 library(shiny)
+library(shinydashboard)
+library(readr)
+library(dplyr)
 library(tidyverse)
-library(maps)
+library(devtools)
+library(DT)
 
+#read in dataset
+data <- as_tibble(read_csv("2016.csv"))
+pdata <- as_tibble(read_csv("2016.csv"))
 
+#Create the basis for our dashboard
+dashboardPage(skin = "green",
+              dashboardHeader(title = "World Happiness", titleWidth = "250"),
+              dashboardSidebar(
+                  sidebarMenu(
+                      menuItem("Information - Start Here!", tabName = "Information", icon = icon("cat")),
+                      menuItem("Data Page", tabName = "Data", icon=icon('chart-bar')),
+                      menuItem("Data Exploration", tabName = "Exploration", icon=icon("arrow-down")),
+                      menuItem("Unsupervised Learning", tabName = "PCAnalysis", icon=icon('connectdevelop')),
+                      menuItem("Modeling Page", tabName = "Models", icon=icon('feather-alt'))
+                  )
+              ),
+              
+              dashboardBody(
+                  tabItems(
+                      tabItem(tabName = "Information", em(h2("Describing the App and Data", style="color:navy")),
+                              box(background = "purple", width=12, 
+                                  h4("Final Project for Lucy Eckert in ST558")),
+                              box(background = "teal", width=12, h5("Final Project for Lucy Eckert in ST558")), 
+                              box(background = "yellow", width=12, h5("Links to Documentation!"), uiOutput("tab"), uiOutput("datatab"))), 
+                      
+                      tabItem(tabName = "PCAnalysis", strong(em(h2("Unsupervised Learning"))),
+                              fluidRow(
+                                  box(background="green", width=10, collapsible = TRUE, collapsed=TRUE, 
+                                      title="About PCA", h4("Principal Component Analysis, also known as PCA, is...")),
+                                  
+                                  selectizeInput("PCs", "Choose the principal components", choices = names(pdata[c(4,5,6,7,8,9,10)])),
+                                  
+                                  selectizeInput("cauze", "Look to see biplot based on causes", choices = levels(as.factor(pdata$Region))),
+                                  uiOutput("biplottext"),
+                                  br(), 
+                                  box(title = "BiPlot", width = 6, plotOutput("Biplot")))),
+                      
+                      tabItem(tabName = "Models", strong(em(h2("Data Modeling"))), 
+                              
+                              box(title="About", h4("We will plot 2 types of supervised models: a simple linear regression model and a multiple linear regression model. 
+                                      For a simple linear regression model, note that we have one predictor value that helps us estimate a response. 
+                                      To help us determine whether the predictor is useful, we can generally look at the R-squared value, which is a measure of correlation. 
+                                      For multiple linear regression, we have more than one predictor value, or we can have a combination of multiple values as well. The R-square here 
+                                      represents the relationship between our predictors and their impact on the response. Below you can see examples of the models written in math form."), 
+                                  solidHeader = TRUE, collapsible = TRUE, collapsed = TRUE, width=10, background = "green", 
+                                  withMathJax(), helpText(h4('An example of a simple linear reg. model - 
+                                           $$y = b_1x_1 + e$$')), 
+                                  helpText(h4('An example of a multiple linear reg. model - 
+                                $$y = b_1x_1 + b_2x_2 + b_3x_1x_2 + e$$'))),
+                              
+                              selectizeInput("slrmodel", "Simple Linear Reg. Model - choose the explanatory variable to visualize relation to the death rate", names(pdata)[c(4,7)]),
+                              uiOutput("modeltext"),
+                              br(),
+                              plotOutput("plot1"),
+                              
+                              sliderInput("Health", "Predict Death Rate by Number of Deaths", min=1000, max=50000, value=10000), 
+                              
+                              tableOutput("mtable"),
+                              
+                              radioButtons("mlrpreds", "Choose the mlr predictors", 
+                                           choices=c("Effect of Meow and Meow on Happiness", 
+                                                     "Effect of Meow and Meow on Happiness", 
+                                                     "Effect of Meow and Meow on Happiness")),
+                              
+                              tableOutput("mlrmodel")),
+                      
+                      tabItem(tabName = "Data", strong(em(h2("Data Tables"))),
+                              
+                              fluidRow(box(title="Expand for More Info", solidHeader = TRUE, collapsible = TRUE, collapsed = TRUE, width = 10, background="green",
+                                           h5("In this app, we explore the factors that were analysed in a study that examined what makes populations happy. This data was ")),
+                                       
+                                       selectizeInput("cause", "Causes", choices = levels(as.factor(pdata$Region))),
+                                       downloadButton("DownloadData", "Download", "Data"),
+                                       hr(), 
+                                       dataTableOutput("Tables"))),
+                      
+                      tabItem(tabName = "Exploration", h2(uiOutput("title")),
+                              column(width=5, 
+                                     box(title="About", solidHeader = TRUE, collapsible = TRUE, collapsed = TRUE, width=10, background = "green", 
+                                         h5("In this app, we are looking at filtered data for all 50 States across from 2010 to 2015. By selecting the 
+                       from the causes of death in the dropdown menu, we can view how the number of deaths have impacted the death rate in the five year interval. We can visualize
+                       this in a plot, we can visualize the average of the deaths compared to the death rate, and we can also see a subset of our data of interest in a table."))),
+                              
+                              fluidRow(
+                                  selectizeInput("causes", "Causes", choices = levels(as.factor(pdata$Region))),
+                                  
+                                  plotOutput("dataplot", click = "plot_click", width = "500", height = "350px"),
+                                  br(), 
+                                  downloadButton("DownloadPlot", "Download Plot"),
+                                  hr(), 
+                                  sliderInput("size", "Size of Points on Graph",
+                                              min = 1, max = 10, value = 3, step = 1),
+                                  
+                                  checkboxInput("check", "Reset All Entries"),
+                                  
+                                  verbatimTextOutput("info"),
+                                  
+                                  box(h2("Numeric Summary of Data"), textOutput("text")),
+                                  
+                                  box(dataTableOutput("table"), title="Click to see table of plot data", solidHeader = TRUE, collapsible = TRUE, collapsed = TRUE))
+                      )
+                  )
+              )
+)
 
-shinyUI(
-    dashboardPage(skin = "yellow",
-        dashboardHeader(title = "What Brings Happiness? A Data Exploration", 
-                        titleWidth = "520"),
-        dashboardSidebar(
-            menuItem("Information",
-                     tabName = "info",
-                     icon = icon("cat")),
-br(),
-            menuItem("Data Exploration",
-                     tabName = "explore",
-                     icon = icon("chart-bar")),
-br(),
-            menuItem("Clustering",
-                     tabName = "cluster",
-                     icon = icon("connectdevelop")),
-br(),            
-            menuItem("Modeling",
-                     tabName = "modeling",
-                     icon = icon("feather-alt")),
-br(),                     
-            menuItem("Access Data",
-                     tabName = "access",
-                     icon = icon("arrow-down"))
-        ),
-        dashboardBody(
-            tabItems(
-                
-                tabItem(tabName = "info",
-                        em(h3("Describing the App and Data", style="color:navy")),
-                        box(background = "purple", width=50,
-                            h5("This dataset blah blah")),
-                        box(background = "green", width=50,
-                            h5("Final Project for Lucy Eckert in ST558. Add some sappy shit
-                               about happiness here.")),
-                        box(background = "fuchsia", width=50,
-                            h5("Supporting Info"), uiOutput("tab"), uiOutput("datatab")),
-                        box(background = "teal", width=50,
-                            h5("Here is how you navigate this app..."))),
-                
-                tabItem(tabName = "explore", width=50,
-                        
-                        fluidRow(box(plotOutput("boxplot"))),
-                        box(selectInput("choice", "Select Region to Summarize ", choices = levels(as.factor(happydata$Region)))),
-                        box(h5(textOutput("info")))),
-                
-                tabItem(tabName = "cluster"),
-                
-                tabItem(tabName = "modeling"),
-                
-                tabItem(tabName = "access", strong(em("Select and Download Data by Region")),
-                    selectizeInput("region", "Region", choices = levels(as.factor(happydata$Region))),
-                    downloadButton("downloadData", "Download"),
-                    hr(),
-                    dataTableOutput("happytable"))
-                )
-            )
-        )
-           
-    )
-    
 
